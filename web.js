ed94f3d7fd7166a95ccd38b60746ec985307566c
Ubuntu <ubuntu@ip-172-31-18-15.us-west-2.compute.internal>
2013-07-12 04:30:32 +0000

    removed buffer nonsense.

--- web.js
+++ web.js
@@ -4,10 +4,11 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  fs.readFileSync('index.html', function (err, data) {
+  /*fs.readFileSync('index.html', 'utf8', function (err, data) {
     if (err) throw err;
     response.send(data.toString("utf8"));
-  });
+  });*/
+  response.send(fs.readFileSync('index.html', 'utf8'));
   //response.send('Hello World 2!');
 });
 
