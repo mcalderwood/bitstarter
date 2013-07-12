714cc97cdcbf6d3eb99917fe0a762cca01c42590
Ubuntu <ubuntu@ip-172-31-18-15.us-west-2.compute.internal>
2013-07-12 04:12:03 +0000

    fixed missing close brace and parenthesis.

--- web.js
+++ web.js
@@ -7,6 +7,7 @@ app.get('/', function(request, response) {
   fs.readFileSync('index.html', function (err, data) {
     if (err) throw err;
     response.send(data.toString("utf-8"));
+  });
   //response.send('Hello World 2!');
 });
 
